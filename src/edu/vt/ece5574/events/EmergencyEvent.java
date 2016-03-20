17caeb11450dd8c76e8c31d4b3daa1f571f3f956
David Kindel <dkindel@vt.edu>
2016-03-20 18:07:27 -0400

    Adding event types

---
+++ src/edu/vt/ece5574/events/EmergencyEvent.java
@@ -0,0 +1,16 @@
+package edu.vt.ece5574.events;
+
+import edu.vt.ece5574.sim.Simulation;
+import sim.engine.*;
+
+public abstract class EmergencyEvent implements Steppable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1;
+
+	public EmergencyEvent(){
+		
+	}
+}
---
+++ src/edu/vt/ece5574/events/IntruderEvent.java
@@ -0,0 +1,18 @@
+package edu.vt.ece5574.events;
+
+import sim.engine.SimState;
+
+public class IntruderEvent extends EmergencyEvent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1;
+
+	@Override
+	public void step(SimState arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
