ef1b79017eb9a7d8f757108fcc01f982086de627
David Kindel <dkindel@vt.edu>
2016-03-20 16:36:40 -0400

    Modified main to actually be able to run.

--- src/edu/vt/ece5574/sim/Configuration.java
+++ src/edu/vt/ece5574/sim/Configuration.java
@@ -1,7 +1,19 @@
 package edu.vt.ece5574.sim;
 
+//Maybe we shouldn't do json or XML?  There's a java properties file format
+//that might be of interest: 
+//http://cs.gmu.edu/~eclab/projects/mason/extensions/webtutorial1/#param-file
+//Honestly, that seems like a smart decision but it's up to the developer
+//
+//You should instantiate agents here on loading the config and provided a way 
+//to get them back to main through function calls of some sort
+
+
 public class Configuration {
 	
+	/**
+	 * 
+	 */
 	public Configuration(){
 		
 	}
--- src/edu/vt/ece5574/sim/Simulation.java
+++ src/edu/vt/ece5574/sim/Simulation.java
@@ -2,26 +2,45 @@ package edu.vt.ece5574.sim;
 
 import edu.vt.ece5574.dummyclasses.DummyAgent;
 import sim.engine.*;
+import sim.field.continuous.Continuous2D;
 
 public class Simulation extends SimState {
 
 	//used to seed the RNG
     private static final long serialVersionUID = 1;
+    public Continuous2D room = new Continuous2D(1.0,100,100);
+    public int numRobots = 5;
 
     public Simulation(long seed){
-    	super(seed);
+    	super(seed); //needs to be first line, can't just set seed here
+    	
+        Configuration config = new Configuration();
+        config.load("config.json");
+        boolean debug = true; //will be read in from config.  If it's a debug, we'll set seed manually
+        seed = 5;
+        if(debug){
+        	if(seed == 0){
+        		System.err.println("Seed cannot be set to 0. Running sim with default seed.");
+        	}
+        	else{
+        		setSeed(seed);
+            	System.out.println("seed modified to: " + seed());
+            	System.out.println("Ignore other message noting the job number and seed value.");
+        	}
+        }
+        
     }
     
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		doLoop(Simulation.class, args);
+		System.exit(0);
 	}
 
 
     public void start() {
         super.start();  // very important!  This resets and cleans out the Schedule.
-        Configuration config = new Configuration();
-        config.load("config.json");
-        schedule.scheduleRepeating(new DummyAgent());
+        room.clear();
+
+        //schedule.scheduleRepeating(new DummyAgent());
     }
 }
